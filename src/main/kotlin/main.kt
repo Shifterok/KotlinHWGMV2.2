0623780e54860fd978d683ecfc2b23b7d58cff4b
Shifterok <Lokky123@mail.ru>
2021-10-17 16:35:39 +0300

    HW2.2 done

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
+
 fun main(){
-    
+    println("Укажите сколько лайков?")
+    val likes = readLine()!!.toInt()
+    if (likes % 2 == 0) {
+        println("Людям")
+    } else println("Человеку")
+
 }
